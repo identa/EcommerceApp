3d745dfc30c4671dad1a6982098f8d481b604cd0
tatrunganh1996 <tatrunganha28@gmail.com>
2019-04-26 16:02:12 +0700

    add paypal payment successfully but need to add more info to paypal activity intent

--- app/src/main/java/com/example/ecommerceapp/DeliveryActivity.java
+++ app/src/main/java/com/example/ecommerceapp/DeliveryActivity.java
@@ -1,5 +1,9 @@
 package com.example.ecommerceapp;
 
+import android.app.Activity;
+import android.app.Dialog;
+import android.content.Intent;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
@@ -7,11 +11,23 @@ import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.ecommerceapp.adapters.CartAdapter;
 import com.example.ecommerceapp.models.CartItemModel;
+import com.paypal.android.sdk.payments.PayPalConfiguration;
+import com.paypal.android.sdk.payments.PayPalPayment;
+import com.paypal.android.sdk.payments.PayPalService;
+import com.paypal.android.sdk.payments.PaymentActivity;
+import com.paypal.android.sdk.payments.PaymentConfirmation;
 
+import org.json.JSONException;
+
+import java.math.BigDecimal;
 import java.util.List;
 
 public class DeliveryActivity extends AppCompatActivity {
@@ -19,7 +35,14 @@ public class DeliveryActivity extends AppCompatActivity {
     private RecyclerView deliveryRecyclerView;
     private Button changeOrAddNewAddressBtn;
     public static List<CartItemModel> cartItemModelList;
-
+    private TextView totalAmount;
+    private TextView fullName;
+    private TextView fullAddress;
+    private Button continuewBtn;
+    private Dialog loadingDialog;
+    private Dialog paymentMethodDialog;
+    private ImageButton paytm;
+    private static final int REQUEST_CODE = 0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -32,6 +55,23 @@ public class DeliveryActivity extends AppCompatActivity {
 
         deliveryRecyclerView = findViewById(R.id.delivery_recycler_view);
         changeOrAddNewAddressBtn = findViewById(R.id.change_or_add_address_btn);
+        totalAmount = findViewById(R.id.total_cart_amount);
+        fullName = findViewById(R.id.full_name);
+        fullAddress = findViewById(R.id.address);
+        continuewBtn = findViewById(R.id.cart_continue_btn);
+
+        Intent intent = new Intent(this, PayPalService.class);
+        intent.putExtra(PayPalService.EXTRA_PAYPAL_CONFIGURATION, configuration);
+        startService(intent);
+        //loading 76 - 12:04
+
+        //payment
+        paymentMethodDialog = new Dialog(DeliveryActivity.this);
+        paymentMethodDialog.setContentView(R.layout.payment_method);
+        paymentMethodDialog.setCancelable(true);
+        paymentMethodDialog.getWindow().setBackgroundDrawable(getDrawable(R.drawable.slider_background));
+        paymentMethodDialog.getWindow().setLayout(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+        //payment
 
         LinearLayoutManager layoutManager = new LinearLayoutManager(this);
         layoutManager.setOrientation(LinearLayoutManager.VERTICAL);
@@ -42,6 +82,55 @@ public class DeliveryActivity extends AppCompatActivity {
         cartAdapter.notifyDataSetChanged();
 
         changeOrAddNewAddressBtn.setVisibility(View.VISIBLE);
+        //part 76 - 2:33
+
+        continuewBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                paymentMethodDialog.show();
+                paytm = paymentMethodDialog.findViewById(R.id.paytm);
+
+                paytm.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        paymentMethodDialog.dismiss();
+                        PayPalPayment payment = new PayPalPayment(new BigDecimal(cartItemModelList.get(cartItemModelList.size() - 1).getTotalItemPrice()), "USD", "Anh", PayPalPayment.PAYMENT_INTENT_SALE);
+
+                        Intent paypalIntent = new Intent(DeliveryActivity.this, PaymentActivity.class);
+                        paypalIntent.putExtra(PaymentActivity.EXTRA_PAYMENT, payment);
+                        paypalIntent.putExtra(PayPalService.EXTRA_PAYPAL_CONFIGURATION, configuration);
+                        startActivityForResult(paypalIntent, REQUEST_CODE);
+                    }
+                });
+            }
+        });
+    }
+
+    @Override
+    protected void onDestroy() {
+        stopService(new Intent(DeliveryActivity.this, PayPalService.class));
+        super.onDestroy();
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+            if (resultCode == Activity.RESULT_OK){
+                PaymentConfirmation confirmation = data.getParcelableExtra(PaymentActivity.EXTRA_RESULT_CONFIRMATION);
+                if (confirmation != null){
+                    try {
+                        String paymentDetails = confirmation.toJSONObject().toString(4);
+                        Toast.makeText(this, "Payment is successful", Toast.LENGTH_SHORT).show();
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+                }
+            } else if (requestCode == Activity.RESULT_CANCELED){
+                Toast.makeText(this, "Payment is cancelled", Toast.LENGTH_SHORT).show();;
+            }
+            else if (requestCode == PaymentActivity.RESULT_EXTRAS_INVALID){
+                Toast.makeText(this, "Invalid payment", Toast.LENGTH_SHORT).show();;
+            }
+        super.onActivityResult(requestCode, resultCode, data);
     }
 
     @Override
@@ -53,4 +142,8 @@ public class DeliveryActivity extends AppCompatActivity {
         }
         return super.onOptionsItemSelected(item);
     }
+
+    private static PayPalConfiguration configuration = new PayPalConfiguration()
+            .environment(PayPalConfiguration.ENVIRONMENT_SANDBOX)
+            .clientId("ATn5fXIAjF-ITaCoG5AnlDL2B4NqwzVgJElPov-HzAlqBNRrQy2LEOPQWjgCVlXla7cp-_GCO1esALmv");
 }
